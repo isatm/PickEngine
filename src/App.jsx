cc613216b3fe6a438f278d7d5281b52524adc14a
Juanma El Mejor <juanma.correo2006@gmail.com>
2024-10-03 16:30:29 -0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from './components/header'
 import './App.css'
+import Header2 from './components/header2'
 
 function App() {
   return(
     <>
-    <Header/>
+    <Header2/>
     </>
   ) 
 }
---
+++ src/components/header2.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header2.css'; // Tu archivo CSS
+import Logo from '/pickEngine2 (1).png';
+
+const Header2 = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <a href="home"><img src={Logo} alt="Logo" className="logo" /></a>
+      </div>
+      <div className="nav-items">
+        <p className="nav-item">IA</p>
+        <p className="nav-item">Forum</p>
+        <p className="nav-item">3D Model</p>
+      </div>
+      <div className="login-register-container">
+        <a href="login"><button type="button" className="btn-1">Login</button></a>
+        <a href="register"><button type="button" className="btn-1">Sign-up</button></a>
+      </div>
+    </header>
+  );
+};
+
+export default Header2;
