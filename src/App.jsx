62f6fec9b68c947a4d87d3c926715bee4611458e
Juanma El Mejor <juanma.correo2006@gmail.com>
2024-10-03 19:43:43 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/header'
 import './App.css'
 import Header2 from './components/header2'
+import Footer from './components/footer'
 
 function App() {
   return(
     <>
     <Header2/>
+    <Footer/>
     </>
   ) 
 }
