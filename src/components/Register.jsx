7aace4bf5707e3f092a2305e3770babe7b4f0694
Stiven Alejandro Carvajal Perez <stivena.carvajalp@autonoma.edu.co>
2024-10-04 01:51:10 -0500

    register completo

---
+++ src/components/Register.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import fondo from '../assets/register-background.jpg';
+import './Register.css';
+
+export function Register() {
+    return (
+    <div className="register-container">
+        <img src={fondo} alt="Imagen de fondo para el registro" className="register-background" />
+        <div className="register-form-container">
+            <h1>Crear cuenta</h1>
+            <form className="register-form">
+                <div className="form-group">
+                    <label htmlFor="nickname">Nickname</label>
+                    <input type="text" id="nickname" placeholder="Ingrese su nickname" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" placeholder="Ingrese su correo electrónico" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" placeholder="Ingrese su contraseña" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirm-password">Confirmar Password</label>
+                    <input type="password" id="confirm-password" placeholder="Confirme su contraseña" required />
+                </div>
+                <button type="submit" className="register-btn">Registrar</button>
+                <div className="register-form-footer">
+                <p>¿Ya tienes cuenta? <a href="/login">Inicia sesión</a></p>
+                </div>
+            </form>
+
+        </div>
+    </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { Header } from './components/Header'
+import { Register } from './components/Register'
+
+const router = createBrowserRouter([
+  { path: "/", element: <Header /> },
+  { path: "/register", element: <Register /> },
+])
+
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
