24a75b4253c76f78078a48e59ca9baac6c31bd44
Isabela Taborda <isabelataborda2005@gmail.com>
2024-10-03 22:22:09 -0500

    Segunda parte del login se centro el form y se añadieron los registros de google y facebook

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,19 +1,19 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import './login.css';
+import "./login.css";
 
 export default function Body() {
   return (
-    
     <div className="d-flex align-items-center py-4 bg-light body-container">
       <main className="form-signin w-100 m-auto form-container">
         <form>
           <h1 className="h3 mb-3 fw-normal text-center">Inicia Sesion</h1>
           <div className="form-floating mb-3 input-style">
-            <input  
+            <input
               type="email"
               className="form-control"
               id="floatingInput"
-              placeholder="Correo"
+              placeholder="Usuario/Correo"
             />
           </div>
 
@@ -37,10 +37,24 @@ export default function Body() {
               Remember me
             </label>
           </div>
+          <div className="button-container">
+            <button className="btn btn-primary w-100 py-2" type="submit">
+              Sign in
+            </button>
+            <button className="btn btn-primary w-100 py-2" type="submit">
+              Sign up
+            </button>
+          </div>
+          <hr/>     
 
-          <button className="btn btn-primary w-100 py-2" type="submit">
-            Sign in
-          </button>
+          <div className="d-flex justify-content-around">
+            <button className="btn" type="button">
+              <i className="fab fa-facebook-f"></i> 
+            </button>
+            <button className="btn" type="button">
+              <i className="fab fa-google"></i> 
+            </button>
+          </div>
         </form>
       </main>
     </div>
