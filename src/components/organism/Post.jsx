8978c8a21e87b5639f6e53eefc9a717d810a7d25
Jerónimo RD <jeronimoramirezd24@gmail.com>
2024-10-28 10:47:24 -0500

    Se implemento post

--- src/components/organism/Post.jsx
+++ src/components/organism/Post.jsx
@@ -1,16 +1,29 @@
 import './Post.css' 
 
-export default function Post() {
+export default function Post({Name, Text}) {
   return (
     <main className='generalPost'>
         <header className = 'headerPost'>
+          <div className = 'postUsuario'>
+            <img src="Avatar.png" alt="Logo" className="imageAvatar" />
+            <h2>{Name}</h2>
+          </div>
+          <div className = 'postFecha'>
+            <h3>24/10/2024</h3>
+          </div>
         </header>
-        <section>
+        <section className = 'postContenido'>
+          <div className = 'postTexto'>
             <p>
-                Ostion
+              {Text}
             </p>
+          </div>
+          <div className = 'postImagen'>
+            <img src="CarroFotografia.jpg" alt="Logo" className="imagePost" />
+          </div>
         </section>
         <footer className = 'footerPost'>
+          
         </footer>
     </main>
   )
--- src/components/pages/forum.jsx
+++ src/components/pages/forum.jsx
@@ -1,6 +1,8 @@
 import './Forum.css'
 import CustomButton from '../atoms/Button/CustomButton';
 import Chat from '../molecules/Chat/Chat';
+import CustomNav from '../atoms/Nav/CustomNav';
+import Post from '../organism/post';
 
 export default function Forum() {
     return (
@@ -12,6 +14,13 @@ export default function Forum() {
             <Chat/>
             </section>
             <section className = 'publicaciones'>
+            <CustomNav
+                text = {"Crear una nueva publicación"}
+            />
+            <Post
+                Name = "Jero"
+                Text = "Ostias chavales, yo opino que debemos de comprar más bmw para poder dominar el mundo de tal manera que nadie nos detenga."
+            />
             </section>
             <section className = 'etiquetas'>
             </section>
