73ef3632b601016c20b20cc3c9005142678ffb88
Jerónimo RD <jeronimoramirezd24@gmail.com>
2024-10-28 03:15:32 -0500

    Se agregó el atomo mensaje y la molecula chatel organismo post

--- src/components/organism/Post.jsx
+++ src/components/organism/Post.jsx
@@ -1,9 +1,17 @@
 import './Post.css' 
 
-export default function Chat() {
+export default function Post() {
   return (
     <main className='generalPost'>
-        
+        <header className = 'headerPost'>
+        </header>
+        <section>
+            <p>
+                Ostion
+            </p>
+        </section>
+        <footer className = 'footerPost'>
+        </footer>
     </main>
   )
 }
